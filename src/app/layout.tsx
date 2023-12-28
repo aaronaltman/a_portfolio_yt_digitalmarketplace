cd4d568c70dbf659b49177350bfaf62dfde2e19b
aka <aaronkaltman@gmail.com>
2023-12-27 20:59:32 -0500

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Container from "@/components/global/container";
+import NavBar from "@/components/global/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NavBar />
         <Container>{children}</Container>
       </body>
     </html>
